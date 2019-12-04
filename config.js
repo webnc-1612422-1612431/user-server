c187cabbc6418ac73d5b7e8ffb31a966ccf1d0dc
Quang Nghĩa <tonystrinh@gmail.com>
2019-12-04 20:56:16 +0700

    change config for up heroku

--- config.js
+++ config.js
@@ -16,25 +16,25 @@ var exports_2 = {
     'client-domain': '//localhost:3001/',
     'server-domain': '//localhost:3000/',
     'database': {
-        'host': 'sql12.freemysqlhosting.net',
+        'host': 'sql10.freemysqlhosting.net',
         'port': '3306',
-        'user': 'sql12308370',
-        'password': 'xLicY5Js7i',
-        'database': 'sql12308370'
+        'user': 'sql10314363',
+        'password': 'alEh71xttJ',
+        'database': 'sql10314363'
     }
 }
 
 // these exports are for uploading to heroku
 var exports_3 = {
-    'client-domain': '',
-    'server-domain': '',
+    'client-domain': 'https://userclient-422-431.herokuapp.com/',
+    'server-domain': 'https://userserver-422-431.herokuapp.com/',
     'database': {
-        'host': 'sql12.freemysqlhosting.net',
+        'host': 'sql10.freemysqlhosting.net',
         'port': '3306',
-        'user': 'sql12308370',
-        'password': 'xLicY5Js7i',
-        'database': 'sql12308370'
+        'user': 'sql10314363',
+        'password': 'alEh71xttJ',
+        'database': 'sql10314363'
     }
 }
 
-module.exports = exports_1;
+module.exports = exports_3;
